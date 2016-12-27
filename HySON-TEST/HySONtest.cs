1ac6fc2e48302a5d8fb7061b4c909ed2f470ebb3
Sohee <soheee@hanyang.ac.kr>
2016-12-27 09:32:41 +0000

    test classes

---
+++ HySON-TEST/HySONtest.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+
+namespace hySON
+{
+    [TestFixtureAttribute]
+    class HySONTest
+    {
+        [TestAttribute]
+        public void testParseEmptyStringArray()
+        {
+
+        }
+
+        [TestAttribute]
+        public void testParseStringArray()
+        {
+
+        }
+
+        [TestAttribute]
+        public void testParseWithConstructor()
+        {
+
+        }
+
+        [TestAttribute]
+        public void testParseEmptyBooleanArray()
+        {
+
+        }
+
+        [TestAttribute]
+        public void testParseBooleanArray()
+        {
+
+        }
+
+        [TestAttribute]
+        public void testParseEmptyIntArray()
+        {
+
+        }
+
+        [TestAttribute]
+        public void testParseIntArray()
+        {
+
+        }
+
+        [TestAttribute]
+        public void testParseIntWithConstructor()
+        {
+
+        }
+
+        [TestAttribute]
+        public void testParseObject()
+        {
+
+        }
+
+        [TestAttribute]
+        public void testParseObjectWithKey()
+        {
+
+
+        }
+        [TestAttribute]
+        public void testParseObjectInObject()
+        {
+
+        }
+        [TestAttribute]
+        public void testParseObjectArray()
+        {
+
+        }
+        [TestAttribute]
+        public void testParseArrayListInObject()
+        {
+
+        }
+        [TestAttribute]
+        public void testParseMultiArrayListInObject()
+        {
+
+        }
+        [TestAttribute]
+        public void testParseMultMemberInObject()
+        {
+
+        }
+
+
+    }
+}
+
